29f7d6fcbb0ff15a2c884c53cadca5d48c0682ac
Miguel <miguel.srmv@gmail.com>
2024-08-05 12:00:52 +0100

    Added DAP setup

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -162,8 +162,8 @@ return {
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				clangd = {
-          filetypes = {"c", "cpp", "h", "hpp", "objc", "objcpp"}
-        },
+					filetypes = { "c", "cpp", "h", "hpp", "objc", "objcpp" },
+				},
 				-- gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
