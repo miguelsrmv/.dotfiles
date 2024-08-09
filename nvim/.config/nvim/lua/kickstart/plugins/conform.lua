c36f122f075712161f90a43a3d2d559fe5608b23
Miguel Sá <miguel.srmv@gmail.com>
2024-08-09 21:35:53 +0100

    Clang format change

--- nvim/.config/nvim/lua/kickstart/plugins/conform.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/conform.lua
@@ -18,7 +18,7 @@ return {
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = false, cpp = false, tpp = false }
+				local disable_filetypes = { c = true, cpp = true, tpp = false }
 				return {
 					timeout_ms = 500,
 					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -37,7 +37,7 @@ return {
 			},
 			formatters = {
 				["clang-format"] = {
-					prepend_args = { "-style", "{BasedOnStyle: GNU, UseTab: Always, IndentWidth: 4, TabWidth: 4}" },
+					prepend_args = { "-style", "{BasedOnStyle: Google, UseTab: Always, IndentWidth: 4, TabWidth: 4}" },
 				},
 			},
 		},
