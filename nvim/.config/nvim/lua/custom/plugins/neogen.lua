f62410d9af83a2c9ce52ef5c7a5f524a28a50bac
Miguel <miguel.srmv@gmail.com>
2024-12-31 15:29:29 +0000

    Added neogen

---
+++ nvim/.config/nvim/lua/custom/plugins/neogen.lua
@@ -0,0 +1,9 @@
+return {
+  'danymat/neogen',
+  config = true,
+  version = '*',
+  vim.api.nvim_set_keymap('n', '<Leader>cc', ":lua require('neogen').generate()<CR>", { noremap = true, silent = true, desc = 'Generate comment (Neogen)' }),
+  event = 'VeryLazy',
+}
+
+-- vim: ts=2 sts=2 sw=2 et
