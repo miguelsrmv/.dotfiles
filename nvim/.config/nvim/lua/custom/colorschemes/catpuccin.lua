75b665da8dd02b3489c044445872d7d2499feb8c
Miguel <miguel.srmv@gmail.com>
2025-02-19 11:04:58 +0000

    Nightfox

--- nvim/.config/nvim/lua/custom/colorschemes/catpuccin.lua
+++ nvim/.config/nvim/lua/custom/colorschemes/catpuccin.lua
@@ -1,13 +1,5 @@
 return {
   'catppuccin/nvim',
   name = 'catppuccin',
-  priority = 1000,
-  opts = { transparent = true },
-  init = function()
-    -- Load the colorscheme here.
-    vim.cmd.colorscheme 'catppuccin'
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
 }
 -- vim: ts=2 sts=2 sw=2 et
