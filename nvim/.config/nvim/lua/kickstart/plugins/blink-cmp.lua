e92c04a3f8239b83b00c635a9900b51faa81de5f
Miguel Sá <miguel.srmv@gmail.com>
2025-04-29 08:24:27 +0100

    Blink change

--- nvim/.config/nvim/lua/kickstart/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/blink-cmp.lua
@@ -18,16 +18,16 @@ return {
           return 'make install_jsregexp'
         end)(),
         dependencies = {
-          'friendly-snippets',
+          -- 'friendly-snippets',
           -- contains a variety of premade snippets.
           -- See the README about individual language/framework/plugin snippets:
           -- https://github.com/rafamadriz/friendly-snippets
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-          },
+          -- {
+          --   'rafamadriz/friendly-snippets',
+          --   config = function()
+          --     require('luasnip.loaders.from_vscode').lazy_load()
+          --   end,
+          -- },
         },
         opts = {},
       },
